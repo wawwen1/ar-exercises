f04d2d487e3bc8636bf7668cc64c3780eda2b3d0
warren roque <warrenroque17@gmail.com>
2022-05-05 06:48:13 +0000

    add validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+print 'Enter store name: '
+store_name = $stdin.gets.chomp
+@store = Store.create(name: store_name)
+@store.errors.full_messages
